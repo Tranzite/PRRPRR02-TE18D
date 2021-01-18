00baa8c18938facc67365680f8ad8cedc6e9fc18
Tranzite <54800477+Tranzite@users.noreply.github.com>
2021-01-18 12:17:42 +0100

    Labb

--- SOLID-Labb/SOLID-Labb/SOLID-Labb/Program.cs
+++ SOLID-Labb/SOLID-Labb/SOLID-Labb/Program.cs
@@ -6,30 +6,39 @@ namespace SOLID_Labb
     {
         static void Main(string[] args)
         {
-            Animal[] animals = {new Dog("Black"),
-                            new Hedgehog("Pink"),
-                            new Bird("Yellow")};
+
 
             Console.WriteLine("###############");
             Console.WriteLine();
-
-            foreach (var animal in animals)
-            {
-                if (animal is Dog dog)
-                {
-                    dog.TransferOwnership("Niklas");
-                }
-
-                animal.Eat();
-                animal.Sleep();
-                animal.Speak();
-                animal._color = "Grey";
-                Console.WriteLine(animal.GetType().Name + " is " + animal._color);
-                Console.WriteLine();
-
-                Console.WriteLine("###############");
-                Console.WriteLine();
-            }
         }
+
+            static void AnimalColor(string[] args)
+            {
+                Animal[] animals = {new Dog("Black"),
+                            new Hedgehog("Pink"),
+                            new Bird("Yellow")};
+            }
+            static void Print(string[] args, Animal[] animals)
+            {
+                foreach (var animal in animals)
+                {
+                    if (animal is Dog dog)
+                    {
+                        dog.TransferOwnership("Niklas");
+                    }
+
+                    animal.Eat();
+                    animal.Sleep();
+                    animal.Speak();
+                    animal._color = "Grey";
+                    Console.WriteLine(animal.GetType().Name + " is " + animal._color);
+                    Console.WriteLine();
+
+                    Console.WriteLine("###############");
+                    Console.WriteLine();
+                }
+            }
+        
     }
 }
+
